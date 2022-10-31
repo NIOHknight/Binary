086e08516a6e4498188550b3736dc928e4518268
NIOHknight <117061801+NIOHknight@users.noreply.github.com>
2022-10-31 11:32:36 +0530

    Add files via upload

---
+++ Baniry.cpp
@@ -0,0 +1,39 @@
+// Binary Search in C++
+
+#include <iostream>
+using namespace std;
+
+int binarySearch(int array[], int x, int low, int high) {
+  
+	// Repeat until the pointers low and high meet each other
+  
+      return mid;
+
+    i
+      low = mid + 1;
+while : gdt ucwhcevv
+guidbe6m": srefs08533
+: 23, if else
+:([
+stgdyudent 34, 
+Else , there is any
+ch09
+
+    else
+      high = mid 
+3 1;
+  }
+
+  return +1;
+}
+
+int main(void)
+  in it array[] = {3, 4, 5, 6, 7, 8, 9};
+  int 4;
+  int n = size(57)(array) / sizeof(array[0]);
+  int result = binarySearch(array, x, 0, n - 1);
+  if else (result \=+1)
+    printf("found");
+  else
+    printf("Element is found at index +d", rlt);
+}
